cd3d33366049f29d579f77dd3df3da0d66c4f558
Sorin Pavel <sorin.pavel@procesio.com>
2021-10-02 14:59:54 +0300

    DateTime Actions

---
+++ DateTimeActions/UTCNow/UtcNow.cs
@@ -0,0 +1,27 @@
+﻿using Ringhel.Procesio.Action.Core;
+using Ringhel.Procesio.Action.Core.ActionDecorators;
+using Ringhel.Procesio.Action.Core.Models;
+using Ringhel.Procesio.Action.Core.Utils;
+using System;
+using System.Threading.Tasks;
+
+namespace UTCNow
+{
+    [ClassDecorator(Name = "Display UTC", Shape = ActionShape.Circle, Description = "Displays the current UTC", IsTestable = true, Classification = Classification.cat1)]
+    [Permissions(CanDelete = true, CanDuplicate = true, CanAddFromToolbar = true)]
+    public class UtcNow : IAction
+    {
+        #region Properties
+        [FEDecorator(Label = "Output UTC", Type = FeComponentType.Date_Time, RowId = 1, Tab = "Details")]
+        [BEDecorator(IOProperty = Direction.Output)]
+        [Validator(IsRequired = true)]
+        public DateTime OutputModifiedTime { get; set; }
+        #endregion
+        #region Execute
+        public async Task Execute()
+        {
+            OutputModifiedTime = DateTime.UtcNow;
+        }
+        #endregion
+    }
+}
