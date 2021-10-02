ad0f050a2a8ceb9c4d9d825286537926b314e379
Sorin Pavel <sorin.pavel@procesio.com>
2021-10-02 14:23:10 +0300

    Today commit

---
+++ DateTimeActions/Today/Today.cs
@@ -0,0 +1,31 @@
+﻿using Ringhel.Procesio.Action.Core;
+using Ringhel.Procesio.Action.Core.ActionDecorators;
+using Ringhel.Procesio.Action.Core.Utils;
+using System;
+using System.Threading.Tasks;
+
+namespace Today
+{
+   [ClassDecorator(Name = "Today", Shape = ActionShape.Square, Description = "Displays a string that represent the " +
+        "current date", IsTestable = true, Classification = Classification.cat1)]
+    [Permissions(CanDelete = true, CanDuplicate = true, CanAddFromToolbar = true)]
+    public class Today : IAction
+    {
+        #region Properties
+        [FEDecorator(Label = "Output Date", Type = FeComponentType.Date_Time, RowId = 1, Tab = "Configuration Tab")]
+        [BEDecorator(IOProperty = Direction.Output)]
+        [Validator(IsRequired = true)]
+        public DateTime OutputDate { get; set; }
+        #endregion
+
+        public Today(IServiceProvider service) { }
+
+        #region Execute
+        public async Task Execute()
+        {
+            OutputDate = DateTime.Today;
+        }
+    }
+    #endregion
+
+}
