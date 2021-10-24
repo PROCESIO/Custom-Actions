824e742935245bf4b460c06228eb455747c1b244
sorin.pavel <87076541+sorinpavel91@users.noreply.github.com>
2021-10-24 17:59:40 +0300

    Update OrderByHelper.cs

--- ListActions/ReturnNLastElements/OrderByHelper.cs
+++ ListActions/ReturnNLastElements/OrderByHelper.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace list.returnnlastelments
+namespace list.returnnlastelements
 {
 
     public static class OrderByHelper
