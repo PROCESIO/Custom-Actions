6ca382ea737c58183d09665e2f00b459f2249f49
Sorin Pavel <sorin.pavel@procesio.com>
2022-03-15 14:03:12 +0200

    March 15 commit

---
+++ Documents/ExportToCSV/ExportToCSV.cs
@@ -0,0 +1,123 @@
+﻿using CsvHelper;
+using ExportToCSV.Helpers;
+using Ringhel.Procesio.Action.Core;
+using Ringhel.Procesio.Action.Core.ActionDecorators;
+using Ringhel.Procesio.Action.Core.Models;
+using Ringhel.Procesio.Action.Core.Utils;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportToCSV
+{
+    [ClassDecorator(Name = "Export to CSV", Shape = ActionShape.Square,
+       Description = "Exports a list (of primitives/simple or of data model) to a .csv file, with export options",
+       Classification = Classification.cat1, IsTestable = true)]
+    [Permissions(CanDelete = true, CanDuplicate = true, CanAddFromToolbar = true)]
+    public class ExportToCSV : IAction
+    {
+        [FEDecorator(Label = "Object to export", Type = FeComponentType.Text, RowId = 1, Tab = "Configuration")]
+        [BEDecorator(IOProperty = Direction.Input)]
+        [Validator(IsRequired = true)]
+        public IEnumerable<object> ObjectToExport { get; set; }
+
+        [FEDecorator(Label = "Column delimiter", Type = FeComponentType.Text, RowId = 2, Tab = "Configuration",
+            DefaultValue = ",", Tooltip = "Recommended values: ',' (comma) ';' (semicolon) '|' (pipe) or \\t (TAB)")]
+        [BEDecorator(IOProperty = Direction.Input)]
+        [Validator(IsRequired = true, Expects = ExpectedType.String)]
+        public string Delimiter { get; set; }
+
+        [FEDecorator(Label = "Export header", Type = FeComponentType.Check_box, RowId = 3, Tab = "Configuration",
+            DefaultValue = true)]
+        [BEDecorator(IOProperty = Direction.Input)]
+        [Validator(IsRequired = false, Expects = ExpectedType.Boolean)]
+        public bool ExportHeader { get; set; }
+
+        [FEDecorator(Label = "File name", Type = FeComponentType.Text, RowId = 4, Tab = "Configuration",
+            Tooltip = "The .csv file extension will be added automatically")]
+        [BEDecorator(IOProperty = Direction.Input)]
+        [Validator(IsRequired = true, Expects = ExpectedType.String)]
+        public string FileName { get; set; }
+
+        [FEDecorator(Label = "Output file", Type = FeComponentType.File, RowId = 5, Tab = "Configuration")]
+        [BEDecorator(IOProperty = Direction.Output)]
+        [Validator(IsRequired = true)]
+        public FileModel OutputFile { get; set; }
+
+        public ExportToCSV(IServiceProvider service)
+        {
+        }
+
+        public ExportToCSV()
+        {
+        }
+
+        public async Task Execute()
+        {
+            if (ObjectToExport == null)
+            {
+                throw new Exception("Input list is null. Please add items to it.");
+            }
+
+            if (ObjectToExport.Count() == 0)
+            {
+                throw new Exception("Input list is empty. Please add items to it.");
+            }
+
+            //Replace \t string with TAB character
+            if (Delimiter == "\\t") { Delimiter = "\t"; }
+
+            //Remove leading & trailing white spaces
+            FileName = FileName.Trim();
+
+            //If the file name does not end with .csv we add the file extension
+            if (!FileName.EndsWith(".csv"))
+            {
+                FileName = Path.ChangeExtension(FileName, ".csv");
+            }
+
+            await using var stream = new MemoryStream();
+            await using var writer = new StreamWriter(stream, Encoding.UTF8);
+
+            CsvHelper.Configuration.CsvConfiguration csvConfig = new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture);
+            csvConfig.Delimiter = Delimiter;
+            csvConfig.HasHeaderRecord = ExportHeader;
+
+            await using var csv = new CsvWriter(writer, csvConfig);
+
+            foreach (var item in ObjectToExport)
+            {
+                var rObj = new System.Dynamic.ExpandoObject() as IDictionary<string, Object>;
+                if (item.IsJObject())
+                {
+                    //Flaten JSON
+                    Dictionary<string, object> flatenJSONdict = item.ToString().DeserializeAndFlatten();
+
+                    foreach (var flat in flatenJSONdict)
+                    {
+                        rObj.Add(flat.Key, flat.Value);
+                    }
+
+                    csv.WriteRecord(rObj);
+                    csv.NextRecord();
+                }
+                else
+                {
+                    rObj.Add("Column", item);
+
+                    csv.WriteRecord(rObj);
+                    csv.NextRecord();
+                }
+            }
+
+            writer.Flush();
+
+            stream.Seek(0, SeekOrigin.Begin);
+            OutputFile = new FileModel(stream, FileName);
+        }
+    }
+}
---
+++ Documents/ExportToCSV/Helpers/JObjectHelper.cs
@@ -0,0 +1,172 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace ExportToCSV.Helpers
+{
+    public static class JObjectHelper
+    {
+        /// <summary>
+        /// Convert an object to JObject
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static JObject ConvertToJObject(this object input)
+        {
+            return JObject.Parse(input.ToString());
+        }
+
+        /// <summary>
+        /// Convert an object to JArray
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static JArray ConvertToJArray(this object input)
+        {
+            return JArray.Parse(input.ToString());
+        }
+
+        /// <summary>
+        /// Check if an object is JObject
+        /// </summary>
+        /// <param name="inputOb"></param>
+        /// <returns></returns>
+        public static bool IsJObject(this object inputOb)
+        {
+            try
+            {
+                ConvertToJObject(inputOb);
+            }
+            catch
+            {
+                return false; //Something went wrong, cannot return a JObj
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Check if an object is JArray
+        /// </summary>
+        /// <param name="inputOb"></param>
+        /// <returns></returns>
+        public static bool IsJArray(this object inputOb)
+        {
+            try
+            {
+                ConvertToJArray(inputOb);
+            }
+            catch
+            {
+                return false; //Something went wrong, cannot return a JArray
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Verify if 2 JObjects are equals
+        /// </summary>
+        /// <param name="jObj1"></param>
+        /// <param name="jObj2"></param>
+        /// <returns></returns>
+        public static bool EqualsJObj(this JObject jObj1, JObject jObj2)
+        {
+
+            if (jObj1.ToString() == jObj2.ToString()) // eq to string => same order
+            {
+                return true;
+            }
+
+            // Add property Name and property Value into the dictionary
+            var JObj1PropertiesToDict = new Dictionary<string, JToken>(); // InputToBeFound to dictionary
+            foreach (JProperty property in jObj1.Properties())
+            {
+                JObj1PropertiesToDict.Add(property.Name, property.Value);
+            }
+
+            // Verify if each property exists in dictionary
+            foreach (JProperty propertyJObj2 in jObj2.Properties())
+            {
+                if (JObj1PropertiesToDict.ContainsKey(propertyJObj2.Name))
+                {
+                    var searchItemPropertyValue = JObj1PropertiesToDict[propertyJObj2.Name];
+                    JObj1PropertiesToDict.Remove(propertyJObj2.Name); // Remove elements that I've already looked for 
+
+                    if (propertyJObj2.Value.Equals(searchItemPropertyValue))
+                    {
+                        continue;
+                    }
+
+                    if (!searchItemPropertyValue.IsJObject() || !(propertyJObj2.Value).IsJObject())
+                    {
+                        return false;
+                    }
+
+                    var searchItemConverted = ConvertToJObject(searchItemPropertyValue);
+                    var propertyConverted = ConvertToJObject(propertyJObj2.Value);
+                    if (!searchItemConverted.EqualsJObj(propertyConverted))
+                    {
+                        return false;
+                    }
+                }
+                else if (!string.IsNullOrEmpty(propertyJObj2.Value.ToString()))
+                {
+                    return false;
+                }
+            }
+
+            // Could be more values in InputToBeFound than in Input list
+            if (JObj1PropertiesToDict.Count > 0)
+            {
+                var JObj1PropertiesToDictValues = JObj1PropertiesToDict.Values;
+                foreach (var value in JObj1PropertiesToDictValues)
+                {
+                    if (!string.IsNullOrEmpty(value.ToString()))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+
+        public static Dictionary<string, object> DeserializeAndFlatten(this string json)
+        {
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            JToken token = JToken.Parse(json);
+            FillDictionaryFromJToken(dict, token, "");
+            return dict;
+        }
+
+        private static void FillDictionaryFromJToken(Dictionary<string, object> dict, JToken token, string prefix)
+        {
+            switch (token.Type)
+            {
+                case JTokenType.Object:
+                    foreach (JProperty prop in token.Children<JProperty>())
+                    {
+                        FillDictionaryFromJToken(dict, prop.Value, Join(prefix, prop.Name));
+                    }
+                    break;
+
+                case JTokenType.Array:
+                    int index = 0;
+                    foreach (JToken value in token.Children())
+                    {
+                        FillDictionaryFromJToken(dict, value, Join(prefix, index.ToString()));
+                        index++;
+                    }
+                    break;
+
+                default:
+                    dict.Add(prefix, ((JValue)token).Value);
+                    break;
+            }
+        }
+
+        private static string Join(string prefix, string name)
+        {
+            return (string.IsNullOrEmpty(prefix) ? name : prefix + "." + name);
+        }
+    }
+}
